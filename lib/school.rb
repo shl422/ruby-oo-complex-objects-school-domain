7d2a74b80bd98b69de83c6ee9b50df6d485e1ac5
Shell <h.k.domingo@outlook.com>
2022-04-26 16:11:10 +0800

    “Done”

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            students_array = @roster[grade]
+        else
+            students_array = []
+        end    
+        students_array.push(name)
+        @roster[grade] = students_array
+    end
+
+    def grade(grade_num)
+        @roster[grade_num]
+    end
+
+    def sort
+        @roster.each {|key, value| @roster[key] = value.sort}
+    end    
+end
